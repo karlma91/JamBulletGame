dec87f73c2bf176bc5699db32283c9e07762efc0
Karl Kalvik <km.kalvik@gmail.com>
2014-09-30 22:21:38 +0200

    Initial project

---
+++ desktop/src/no/ww/bullettest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package no.ww.bullettest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import no.ww.bullettest.BulletTest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BulletTest(), config);
+	}
+}
